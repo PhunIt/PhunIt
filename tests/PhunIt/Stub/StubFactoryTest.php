034a7f0458f853739c4bf0007466d18d6972b99f
Guillermo <guille@nianoniano.com>
2011-03-20 13:31:43 +0100

    Test RED: Should return an instance of the received class

--- tests/PhunIt/Stub/StubFactoryTest.php
+++ tests/PhunIt/Stub/StubFactoryTest.php
@@ -1,14 +1,23 @@
 <?php
 
 class StubFactory {
+
   public static function create($class) {
-    
+
   }
+
 }
 
 class StubFactoryTest extends PHPUnit_Framework_TestCase {
+
   public function testNeedsAClassNameToCreateAStub() {
     $this->setExpectedException('\Exception');
     $stub = StubFactory::create();
   }
+
+  public function testShouldReturnAStubbedInstaceOfTheClass() {
+    $stub = StubFactory::create('ChuChu');
+    $this->assertTrue($stub instanceof ChuChu);
+  }
+
 }

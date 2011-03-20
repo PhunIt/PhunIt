bbf6a28670a6cd2983df7df1917b94796bfc81d8
Guillermo <guille@nianoniano.com>
2011-03-20 12:18:46 +0100

    Test RED: Needs a method name to stub

--- tests/PhunIt/Stub/StubTest.php
+++ tests/PhunIt/Stub/StubTest.php
@@ -1,5 +1,9 @@
 <?php
 
-class StubTest {
-    
+class StubTest extends \PHPUnit_Framework_TestCase {
+  public function testNeedsAMethodNameToStub() {
+    $this->setExpectedException('\Exception');
+    $stub = new Stub();
+    $stub->stubs();
+  }
 }

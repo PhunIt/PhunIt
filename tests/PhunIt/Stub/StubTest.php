15c697dacf75b9691812f6cb8a685204705af8ed
Guillermo <guille@nianoniano.com>
2011-03-20 12:19:54 +0100

    Test GREEN: Needs a method name to stub

--- tests/PhunIt/Stub/StubTest.php
+++ tests/PhunIt/Stub/StubTest.php
@@ -1,5 +1,11 @@
 <?php
 
+class Stub {
+  public function stubs($method) {
+    
+  }
+}
+
 class StubTest extends \PHPUnit_Framework_TestCase {
   public function testNeedsAMethodNameToStub() {
     $this->setExpectedException('\Exception');

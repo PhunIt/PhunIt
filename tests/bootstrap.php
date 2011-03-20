a18e65ec0d70b2567550943473c9bc10d7afacc8
Guillermo <guille@nianoniano.com>
2011-03-20 15:16:50 +0100

    REFACTOR: Returned class should also be a Stub

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,5 +2,6 @@
 
 require_once __DIR__ . '/../src/PhunIt/Stub/Stubbed.php';
 require_once __DIR__ . '/../src/PhunIt/Stub/Stub.php';
+require_once __DIR__ . '/../src/PhunIt/Stub/Factory.php';
 require_once __DIR__ . '/../src/PhunIt/Method/Method.php';
 require_once __DIR__ . '/../src/PhunIt/Method/Container.php';

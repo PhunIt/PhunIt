2e2a82458169d5339e141c7a494d1e7ecae28c37
Guillermo <guille@nianoniano.com>
2011-03-20 12:21:28 +0100

    Test RED: Should return a Method when stubbing

--- tests/PhunIt/Stub/StubTest.php
+++ tests/PhunIt/Stub/StubTest.php
@@ -1,15 +1,24 @@
 <?php
 
 class Stub {
+
   public function stubs($method) {
     
   }
+
 }
 
 class StubTest extends \PHPUnit_Framework_TestCase {
+
   public function testNeedsAMethodNameToStub() {
     $this->setExpectedException('\Exception');
     $stub = new Stub();
     $stub->stubs();
   }
+
+  public function testShouldReturnAMethodWhenStubbing() {
+    $stub = new Stub();
+    $this->assertTrue($stub->stubs('chuchu') instanceof Method);
+  }
+
 }
